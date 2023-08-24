15e1376430965c2eac083251506a204a34285cde
Ivan <kryamky@yandex.ru>
2023-08-24 16:37:35 +0300

    Start app

---
+++ src/features/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './Header.module.scss'
+import Remove from '../../assets/img/icon-remove.svg'
+
+const Header = () => {
+	return (
+		<div className={style.header}>header <Remove/> </div>
+	)
+}
+
+export default Header
